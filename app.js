abd6722bf025694d3ee6dcef7b9a171c6deb0a42
alkatsalex <alkatsalex@ukr.net>
2024-03-28 01:03:05 +0200

    start

---
+++ app.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+});
+
+export default app;
