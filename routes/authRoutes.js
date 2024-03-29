6d8b520c68303154dcbfe2f69d089a83146f3c35
Igor Konoval <kip_y@sfa.org.ua>
2024-03-29 10:01:41 +0200

    edit registry

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,12 +4,19 @@ import registerUsers from "../controllers/authController/authControllers.js";
 import loginUsers from "../controllers/authController/loginController.js";
 import logOut from "../controllers/authController/logoutController.js";
 import { auth } from "../middleware/auth.js";
+import validateBody from "../middleware/validateBody.js";
+import { userAuth } from "../schemas/userAuth.js";
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
 // import upload from "../middleware/upload.js";
 // import { verify } from "node:crypto";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", registerUsers);
+authRouter.post(
+  "/register",
+  validateBody(userAuth),
+  ctrlWrapper(registerUsers)
+);
 authRouter.post("/login", loginUsers);
 authRouter.get("/logout", auth, logOut);
 
