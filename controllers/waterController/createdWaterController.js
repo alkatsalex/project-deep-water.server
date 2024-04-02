0cefb486d922ee86a5403ccc7c2dede61455700a
alkatsalex <alkatsalex@ukr.net>
2024-04-02 11:00:25 +0300

    done

--- controllers/waterController/createdWaterController.js
+++ controllers/waterController/createdWaterController.js
@@ -7,15 +7,15 @@ const createdWaterController = async (req, res) => {
   const date = new Date(currentData);
   const stringDate = date.toLocaleDateString();
   console.log(stringDate);
-  const water = await WaterTracking.findOne({ date: stringDate });
+  const water = await WaterTracking.findOne({ date: stringDate, user_id: id });
 
   if (water !== null) {
-    return res.send("today is heve water");
+    return res.end();
   }
   const newWater = await WaterTracking.create({
     date: stringDate,
     user_id: id,
   });
-  res.send(newWater);
+  res.status(201).send(newWater);
 };
 export default createdWaterController;
