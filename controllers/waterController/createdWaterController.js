06effac98ff321dcc95fcc94ada047133e9adf47
alkatsalex <alkatsalex@ukr.net>
2024-04-03 16:16:29 +0300

    done

--- controllers/waterController/createdWaterController.js
+++ controllers/waterController/createdWaterController.js
@@ -12,13 +12,32 @@ const createdWaterController = async (req, res) => {
   const water = await WaterTracking.findOne({ date: stringDate, owner: id });
 
   if (water !== null) {
-    return res.end();
+    const count = water.water_entries.length;
+    water.count = count;
+    const totalAmount = water.water_entries.reduce(
+      (acc, entry) => acc + entry.amount,
+      0
+    );
+    const percentage = (totalAmount / water.daily_limit) * 100;
+    water.count = count;
+    water.percent = percentage;
+
+    return res.status(200).send(water);
   }
+
+  // const water_entries = [
+  //   { time: "7:00", amount: 250 },
+  //   { time: "8:00", amount: 250 },
+  //   { time: "9:00", amount: 250 },
+  //   { time: "9:00", amount: 250 },
+  // ];
 
   const newWater = await WaterTracking.create({
     date: stringDate,
     owner: id,
     daily_limit,
+    water_entries,
+    count: water_entries.length,
   });
   res.status(201).send(newWater);
 };
