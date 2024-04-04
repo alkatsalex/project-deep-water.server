9ae590bd7da63f1d9776902a3be8bde905dbea8c
alkatsalex <alkatsalex@ukr.net>
2024-04-04 23:52:19 +0300

    swagger v 3.1.0 and change /edit to /info

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,46 +1,46 @@
 // НІЧОГО НЕ ЗМІНЮВАТИ
 
-import express from 'express';
+import express from "express";
 import {
   registerUsers,
   loginUsers,
   logOut,
   verifyEmail,
   verifyEmailRepeat,
-} from '../controllers/authController/index.js';
+} from "../controllers/authController/index.js";
 import {
   updateAvatar,
   updateInfo,
   getUserInfo,
-} from '../controllers/userController/index.js';
-import { auth, validateBody, upload } from '../middleware/index.js';
-import { emailSchema, userAuth } from '../schemas/userAuth.js';
-import { ctrlWrapper } from '../helpers/index.js';
+} from "../controllers/userController/index.js";
+import { auth, validateBody, upload } from "../middleware/index.js";
+import { emailSchema, userAuth } from "../schemas/userAuth.js";
+import { ctrlWrapper } from "../helpers/index.js";
 
 // import { verify } from "node:crypto";
 
 const authRouter = express.Router();
 
 authRouter.post(
-  '/register',
+  "/register",
   validateBody(userAuth),
   ctrlWrapper(registerUsers)
 );
-authRouter.get('/verify/:verificationToken', ctrlWrapper(verifyEmail));
+authRouter.get("/verify/:verificationToken", ctrlWrapper(verifyEmail));
 authRouter.post(
-  '/verify',
+  "/verify",
   validateBody(emailSchema),
   ctrlWrapper(verifyEmailRepeat)
 );
-authRouter.post('/login', validateBody(userAuth), ctrlWrapper(loginUsers));
-authRouter.get('/logout', auth, ctrlWrapper(logOut));
-authRouter.get('/info', auth, ctrlWrapper(getUserInfo));
+authRouter.post("/login", validateBody(userAuth), ctrlWrapper(loginUsers));
+authRouter.get("/logout", auth, ctrlWrapper(logOut));
+authRouter.get("/info", auth, ctrlWrapper(getUserInfo));
 authRouter.patch(
-  '/avatar',
+  "/avatar",
   auth,
-  upload.single('avatar'),
+  upload.single("avatar"),
   ctrlWrapper(updateAvatar)
 );
-authRouter.patch('/edit', auth, ctrlWrapper(updateInfo));
+authRouter.patch("/info", auth, ctrlWrapper(updateInfo));
 
 export default authRouter;
