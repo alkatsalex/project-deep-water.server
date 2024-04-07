1ad658643d93f63a3abfcec8c0a3c972f58006cc
alkatsalex <alkatsalex@ukr.net>
2024-04-07 23:16:21 +0300

    fix4

--- controllers/waterController/monthController.js
+++ controllers/waterController/monthController.js
@@ -4,16 +4,16 @@ import HttpError from "../../helpers/HttpError.js";
 const getMonthData = async (req, res) => {
   const { id } = res.user;
   const { date } = req.body;
+  console.log("date", date);
+  console.log(typeof date);
 
   if (date === null) {
     throw HttpError(400, "The body must contain a date");
   }
 
-  const stringDate = date.toString();
-
   const result = await WaterTracking.find({
     owner: id,
-    date: { $regex: stringDate },
+    date: { $regex: date },
   });
   res.status(200).send(result);
 };
