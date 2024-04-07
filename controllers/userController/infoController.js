3003561d178c11121501bd1aeb27b599bc2bca96
alkatsalex <alkatsalex@ukr.net>
2024-04-07 18:37:50 +0300

    done

--- controllers/userController/infoController.js
+++ controllers/userController/infoController.js
@@ -3,8 +3,10 @@ import User from "../../models/userModel.js";
 const getUserInfo = async (req, res) => {
   const { id } = res.user;
 
-  const { email, avatarURL, gender, daily_limit } = await User.findById(id);
-  const newData = { email, avatarURL, id, gender, daily_limit };
+  const { email, avatarURL, gender, daily_limit, name } = await User.findById(
+    id
+  );
+  const newData = { email, avatarURL, id, gender, daily_limit, name };
   res.send(newData);
 };
 
