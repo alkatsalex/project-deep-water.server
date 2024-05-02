0c14f5f51b265a849f6192561f29c36bcbd3321c
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2024-05-02 19:53:47 +0200

    done send mail

--- controllers/userController/deleteAccount.js
+++ controllers/userController/deleteAccount.js
@@ -11,22 +11,22 @@ const deleteAccount = async (req, res) => {
   if (user === null) {
     throw HttpError(404, "User not found");
   }
-  // await WaterTracking.deleteMany({ owner: id });
-  // if (user.avatarURL) {
-  //   await deleteImgFromCloudinary(user.avatarURL);
-  // }
+  await WaterTracking.deleteMany({ owner: id });
+  if (user.avatarURL) {
+    await deleteImgFromCloudinary(user.avatarURL);
+  }
 
   const confirmDeleteEmail = {
     to: email,
     subject: "Confirm delete",
     html: `
-      <h3>We are sad that you want to leave us. If you haven't changed your mind yet, click on the link <a target="_blank" href="https://denys90.github.io/tracker-of-water-frontend"></h3>
+      <h3>We are sad that you want to leave us. If you haven't changed your mind yet, click on the link <a target="_blank" href="https://denys90.github.io/tracker-of-water-frontend">Click here</a></h3>
         `,
-    text: `We are sad that you want to leave us. If you haven't changed your mind yet, click on the link `,
+    text: `We are sad that you want to leave us. If you haven't changed your mind yet, click on the link href="https://denys90.github.io/tracker-of-water-frontend"`,
   };
   await sendEmail(confirmDeleteEmail);
 
-  // const result = await User.findByIdAndDelete(id);
-  // res.status(200).send("Account is deleted");
+  const result = await User.findByIdAndDelete(id);
+  res.status(200).send("Account is deleted");
 };
 export default deleteAccount;
